adbc44b4fff2c8d6bba6412252d7b543b89751cf
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2022-02-22 15:11:05 -0500

    contract estates documents person seeder

--- database/seeders/ContractPersonSeeder.php
+++ database/seeders/ContractPersonSeeder.php
@@ -2,17 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\{ Contract, ContractPerson, Person};
 use Illuminate\Database\Seeder;
 
 class ContractPersonSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        
+        $people = Person::all();
+        for ($i=1; $i <= 2 ; $i++) { 
+            $contract = Contract::where('id', $i)->get();
+            foreach ($people as $person){
+                ContractPerson::create([
+                    'contract_id' => $contract[0]->id,
+                    'person_id' => $person->id,
+                    'typeParticipant' => ''
+                ]);
+            }
+        }   
     }
 }
